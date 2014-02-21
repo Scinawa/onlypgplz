400854d52ba962ecfaea306a698ebbdd41145a7f
scinawa <Scinawa>
2014-02-21 03:05:42 +0100

    Added requirements.txt setup.py and certs

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="OnlyPGPlz",
+    version="1.0",
+    description='Get rid of all the unencrypted writer!',
+    url='https://github.com/Scinawa/onlypgplz',
+    author='Scinawa',
+    author_email='alessandro@luongo.pro',
+    install_requires=[
+        "python-daemon",
+    ],
+    # ...
+)
